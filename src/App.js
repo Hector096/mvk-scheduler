809f72fe6d11ed1b7b48be2e948aa027e0e7addb
Hector096 <vv96rca@gmail.com>
2022-02-23 11:13:18 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
-import "./style.css";
-import Basic from '.'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Switch, Route, useLocation,
+} from 'react-router-dom';
+import Login from './components/Login';
+import Appointments from './components/Appointments';
+import Basic from './components/Scheduler';
+import NewAppointment from './components/NewAppointment';
+import EditAppointment from './components/EditAppointment';
+import NewPatient from './components/NewPatient';
+import { clearMessage } from './redux/actions/message';
 
-export default function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  useEffect(() => {
+    dispatch(clearMessage());
+  }, [dispatch, location]);
+
   return (
-    <Basic />
+    <div>
+      <main>
+        <Switch>
+          <Route exact path={['/', '/home']} component={Basic} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/appointments" component={Appointments} />
+          <Route exact path="/appointments/new" component={NewAppointment} />
+          <Route exact path="/appointments/:id" component={EditAppointment} />
+          <Route exact path="/patient/new" component={NewPatient} />
+        </Switch>
+      </main>
+    </div>
   );
-}
+};
+
+export default App;
